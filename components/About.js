dbcd1b327850173614132ed987beb2f4eb910d3e
i-vishi <rr.vishalgaur@gmail.com>
2021-06-30 12:11:25 +0530

    added top info with image

--- components/About.js
+++ components/About.js
@@ -4,19 +4,26 @@ import {
   Typography,
   makeStyles,
   Avatar,
+  Box,
+  Button,
 } from "@material-ui/core";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   topGrids: {
-    marginTop: theme.spacing(12),
+    marginTop: theme.spacing(14),
   },
   myImage: {
-    width: theme.spacing(48),
-    height: theme.spacing(48),
+    minHeight: theme.spacing(64),
+    minWidth: theme.spacing(64),
   },
   imageGrid: {
-    textAlign: "right",
+    height: "100%",
+  },
+  buttons: {
+    textTransform: 'none',
+    marginTop: theme.spacing(12),
+    fontSize: "1.25em"
   },
 }));
 export default function About() {
@@ -26,21 +33,26 @@ export default function About() {
     <Container>
       <Grid container spacing={2} className={classes.topGrids}>
         <Grid item xs={12} lg={8}>
-          <Typography variant="h5">Hi, I am</Typography>
-          <Typography variant="h1">Vishal Gaur.</Typography>
-          <Typography variant="h2">I build amazing softwares.</Typography>
-          <Typography variant="h6">
-            I am a Software Engineer specialized in Web Development and Android
-            Development with Kotlin. Currently, I am SDE at Paytm(One97
-            Communications Ltd.) working with their Android Team.
-          </Typography>
+          <Box>
+            <Typography variant="h5">Hi, I am</Typography>
+            <Typography variant="h1">Vishal Gaur.</Typography>
+            <Typography variant="h2">I build amazing softwares.</Typography>
+            <Typography variant="h6">
+              I am a Software Engineer specialized in Web Development and
+              Android Development with Kotlin. Currently, I am SDE at
+              Paytm(One97 Communications Ltd.) working with their Android Team.
+            </Typography>
+            <Button variant="outlined" className={classes.buttons} color="secondary">Get In Touch</Button>
+          </Box>
         </Grid>
-        <Grid item className={classes.imageGrid}>
-          <Avatar
-            alt="Vishal Gaur"
-            src="/assets/vg-image.png"
-            className={classes.myImage}
-          />
+        <Grid item xs={12} lg={4}>
+          <Box display="flex" justifyContent="flex-end" alignItems="center" className={classes.imageGrid}>
+            <Avatar
+              alt="Vishal Gaur"
+              src="https://github.com/i-vishi.png?size=500"
+              className={classes.myImage}
+            />
+          </Box>
         </Grid>
       </Grid>
     </Container>
