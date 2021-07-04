840fc38f883eb13e105f0a24e42b36f73f2768c0
i-vishi <rr.vishalgaur@gmail.com>
2021-07-04 13:07:48 +0530

    changed scrollbar style

--- components/theme.js
+++ components/theme.js
@@ -26,6 +26,22 @@ const theme = createMuiTheme({
     fontSize: 16,
     fontFamily: "Oxygen, Roboto, Helvetica, Arial, sans-serif",
   },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*::-webkit-scrollbar': {
+          width: '0.4em'
+        },
+        '*::-webkit-scrollbar-track': {
+          '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          backgroundColor: 'rgba(0,245,255,0.5)',
+          outline: '1px solid slategrey'
+        }
+      }
+    }
+  }
 });
 
 export default theme;
