49000b1fadf875ee51d44c91370845a9d4b60440
vishalGaur <vishal.gaur@testbook.com>
2024-07-08 17:54:11 +0530

    adds lazy loading image

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,7 @@ function MyApp({ Component, pageProps }) {
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
+    // const jssStyles = document.querySelector("#jss-server-side"); if (jssStyles) { //   jssStyles.parentElement.removeChild(jssStyles); // }
   }, []);
 
   return (
